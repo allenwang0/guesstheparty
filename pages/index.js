5719657fc1c787cdc183084934a7bab54b1b9bce
allenwang0 <40436312+allenwang0@users.noreply.github.com>
2025-12-19 14:11:35 -0800

    Add icon

--- pages/index.js
+++ pages/index.js
@@ -745,6 +745,10 @@ export default function Home() {
       <Head>
         <title>Guess The Party | Allen Wang</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🇺🇸</text></svg>"
+        />
       </Head>
 
       <Analytics />
